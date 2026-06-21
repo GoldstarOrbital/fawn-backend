52b405098b42fc4cf32a4fd1acc4b8438438fc1e
Alex Goldsmith <alexmarcusgoldsmith@gmail.com>
2026-06-21 03:31:39 -0700

    Add 7 new schools to valid-schools list for deal suggestions (Sierra, Sac State, Yuba College, Cal Poly SLO, UC Davis, CSU Monterey Bay, UCSB)

--- routers/deals.py
+++ routers/deals.py
@@ -34,7 +34,10 @@ limiter = Limiter(key_func=get_remote_address)
 
 API_KEY_HEADER = APIKeyHeader(name="X-Admin-Key", auto_error=False)
 
-VALID_SCHOOLS = {"berkeley", "stanford", "scu", "sjsu", "usf", "sfstate", "smc", "uoregon"}
+VALID_SCHOOLS = {
+    "berkeley", "stanford", "scu", "sjsu", "usf", "sfstate", "smc", "uoregon",
+    "sierra", "sacstate", "yubacollege", "calpoly", "ucdavis", "csumb", "ucsb",
+}
 VALID_CATEGORIES = {"gas", "food", "coffee", "housing", "bars", "bulk", "coupons"}
 
 
